c1c0d8ebea16fdd819d311bfd2282e1bc6bcbea4
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-06-30 20:47:47 -0400

    Amended June30/2015

--- MovieListApp Lab 19/src/model/Movie.java
+++ MovieListApp Lab 19/src/model/Movie.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Movie {
+public class Movie implements Comparable<Movie>{
 	public String title;
 	public String category;
 	
@@ -12,6 +12,11 @@ public class Movie {
 	@Override
 	public String toString() {
 		return "Movie [title=" + title + ", category=" + category + "]";
+	}
+
+	@Override
+	public int compareTo(Movie m) {		
+		return this.title.compareToIgnoreCase(m.title);		
 	}
 	
 
